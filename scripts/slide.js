36bd5a3249cef872819d577288b2be64b30ae5dd
Tatsuya Namikwa <m757e888a7n7r@gmaiil.com>
2023-03-14 00:03:30 +0900

    3/13 ポートフォリオサイト作成開始　見出しがスライドしてくる

---
+++ scripts/slide.js
@@ -0,0 +1,19 @@
+$(function(){
+    $('document').ready(function(){
+      $('h1').toggleClass('slide_on');
+    }); 
+  });
+
+// 画像をふわっと
+  $(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
