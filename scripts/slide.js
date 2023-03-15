ccbea6fff4ed5e8063c486090004468b1473f0fd
Tatsuya Namikwa <m757e888a7n7r@gmaiil.com>
2023-03-16 01:07:12 +0900

    3/15 portfolio修正　画像ふわっと無し

--- scripts/slide.js
+++ scripts/slide.js
@@ -5,16 +5,3 @@ $(function(){
     }); 
   });
 
-// 画像をふわっと
-  $(function(){
-    $(window).scroll(function (){
-        $('.fadein').each(function(){
-            var elemPos = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scroll > elemPos - windowHeight + 200){
-                $(this).addClass('scrollin');
-            }
-        });
-    });
-});
